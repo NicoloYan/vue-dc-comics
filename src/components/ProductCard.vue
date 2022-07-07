e73b168df7f466c48b7d9c618bc56260580bc06a
NicoloYan <nico.yan@outlook.it>
2022-07-07 15:38:39 +0200

    ContentSection dynamically populated

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="card"> 
+        <img :src="productInfo.thumb" :alt="productInfo.series">
+        <h3>{{ productInfo.series }}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductCard',
+    props: {
+        productInfo: Object
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+    background-color: white;
+    width: calc((100% / 6 ) - 15px);
+    margin: 30px 0;
+}
+img {
+    width: 100%;
+}
+</style>
